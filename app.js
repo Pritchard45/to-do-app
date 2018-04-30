936d9c7c0f01533ee2ad15eb51ec3eea4dc4c408
Alex Pritchard <alexpritchard45@gmail.com>
2018-04-30 11:52:44 -0400

    fixed syntax errors

---
+++ app.js
@@ -0,0 +1,29 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let title = newToDoText.value;
+
+    let newLi = document.createElement('li');
+
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+     // set the title
+    newLi.textContent = title;
+     // attach the checkboxto the li
+    newLi.appendChild(checkbox);
+     // attach the li to the ul
+    toDoList.appendChild(newLi);
+     //empty the input
+    newToDoText.value = '';
+
+  });
+}
+window.onload = function() {
+  onReady();
+}
