4dce876088e2262291c27f5f3e90eaf4bfff7e4a
Alex Pritchard <alexpritchard45@gmail.com>
2018-04-30 01:15:36 -0400

    new checkpoint work for part 2

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
 function onReady(){
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('sumbit', event => {
-    event.preventDefault();
+  function createNewToDo() {
+    if (!newToDoText.value) { return; }
 
-    // get the text
-    let title = newToDoText.value = '';
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
-    // create a new li
-    let newLi= document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkboxto the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
     newToDoText.value = '';
 
+    renderTheUI();
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
 
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo){
+      const newToDo = document.createElement('li');
+
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      title.textContent = toDo.title;
+
+      toDoList.appendChild(newToDo);
+      newToDo.appendChild(checkbox);
+    });
+  }
+  addToDoForm.addEventListener('sumbit', event => {
+    event.preventDefault();
+    createNewToDo();
+    newToDoText.value = '';
   });
 
+  renderTheUI();
+}
 
-};
 
 window.onload = function() {
   onReady();
