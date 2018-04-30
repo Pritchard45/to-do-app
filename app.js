454766cf6276efad85bcd6207e668726c2b8f1f5
Alex Pritchard <alexpritchard45@gmail.com>
2018-04-30 00:49:33 -0400

    update

---
+++ app.js
@@ -0,0 +1,46 @@
+function onReady(){
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('sumbit', event => {
+    event.preventDefault();
+
+    // get the text
+    let title = newToDoText.value = '';
+  
+    // create a new li
+    let newLi= document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkboxto the li
+    newLi.appendChild(checkbox);
+
+    // attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    //empty the input
+    newToDoText.value = '';
+
+
+  });
+
+  function removeElement(elementId) {
+      // Removes an element from the document
+      var element = document.getElementById(elementId);
+      element.parentNode.removeChild(element);
+  };
+
+};
+
+window.onload = function() {
+  onReady();
+};
